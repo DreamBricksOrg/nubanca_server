ac33cfd3b20330763ef248ca80a28c1f42df322d
VitorHugo1901 <vhms48@gmail.com>
2026-09-25 15:04:37 -0300

    fix: adjusted the pages.py to get the env correct

--- app/pages.py
+++ app/pages.py
@@ -1,20 +1,22 @@
 from flask import Blueprint, render_template
 import os
 from dotenv import load_dotenv
+from flask import current_app
+
 bp = Blueprint("pages", __name__, template_folder='../templates')
 
 @bp.get("/editor")
 def open_editor():
-    return render_template('editor.html', name="editor", server_url=os.getenv("BASE_URL"))
+    return render_template('editor.html', name="editor", server_url=current_app.config.get("BASE_URL"))
 
-@bp.get("/termos")
+@bp.get("/")
 def open_terms():
-    return render_template('termos.html', name="termos", server_url=os.getenv("BASE_URL"))
+    return render_template('termos.html', name="termos", server_url=current_app.config.get("BASE_URL"))
 
 @bp.get("/qrcode")
 def open_qrcode():
-    return render_template('qrcode.html', name="qrcode", server_url=os.getenv("BASE_URL"))
+    return render_template('qrcode.html', name="qrcode", server_url=current_app.config.get("BASE_URL"))
 
 @bp.get("/validacao")
 def open_validacao():
-    return render_template('validacao.html', name="validacao", server_url=os.getenv("BASE_URL"))
+    return render_template('validacao.html', name="validacao", server_url=current_app.config.get("BASE_URL"))
